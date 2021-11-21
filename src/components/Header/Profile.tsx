cbaebe6dfc7ad31f8ddf4d526935fbf5b715ab42
mardsonferreira <mardsonferreira25@gmail.com>
2021-11-21 09:25:21 -0300

    Responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Text, Box, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showPrifileData?: boolean;
+}
+
+export function Profile({ showPrifileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Mardson Ferreira</Text>
-                <Text color="gray.300" fontSize="small">
-                    mardsonferreira25@gmail.com
-                </Text>
-            </Box>
+            {showPrifileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Mardson Ferreira</Text>
+                    <Text color="gray.300" fontSize="small">
+                        mardsonferreira25@gmail.com
+                    </Text>
+                </Box>
+            )}
 
             <Avatar
                 size="md"
