f710bfcb7657f89ba1e94d4e67e58b9856a133ed
mardsonferreira <mardsonferreira25@gmail.com>
2021-11-25 20:48:11 -0300

    SignIn form validations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { promises } from "stream";
+import * as yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
 
 import { Input } from "../components/Form/Input";
 
@@ -9,8 +10,16 @@ type SignInFormData = {
     password: string;
 };
 
+const signInFormSchema = yup.object().shape({
+    email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+    password: yup.string().required("Senha obrigatória"),
+});
+
 export default function SignIn() {
-    const { register, handleSubmit, formState } = useForm();
+    const { register, handleSubmit, formState } = useForm({
+        resolver: yupResolver(signInFormSchema)
+    });
+    const { errors} = formState;
 
     const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
         await new Promise(resolve => setTimeout(resolve, 2000));
@@ -34,12 +43,14 @@ export default function SignIn() {
                         label="E-mail"
                         name="email"
                         type="email"
+                        error={errors.email}
                         {...register("email")}
                     />
                     <Input
                         label="Senha"
                         name="password"
                         type="password"
+                        error={errors.password}
                         {...register("password")}
                     />
                 </Stack>
