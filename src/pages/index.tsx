6398afb2ad28dfa358fbfb6110c8fa94ee85b3b8
mardsonferreira <mardsonferreira25@gmail.com>
2021-11-25 20:16:39 -0300

    SignIn authentication form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { promises } from "stream";
+
 import { Input } from "../components/Form/Input";
 
+type SignInFormData = {
+    email: string;
+    password: string;
+};
+
 export default function SignIn() {
+    const { register, handleSubmit, formState } = useForm();
+
+    const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        console.log(data);
+    };
+
     return (
         <Flex w="100vw" h="100vh" align="center" justify="center">
             <Flex
@@ -12,10 +27,21 @@ export default function SignIn() {
                 padding="8"
                 borderRadius={8}
                 flexDirection="column"
+                onSubmit={handleSubmit(handleSignIn)}
             >
                 <Stack spacing="4">
-                    <Input label="E-mail" name="email" type="email" />
-                    <Input label="Senha" name="password" type="password" />
+                    <Input
+                        label="E-mail"
+                        name="email"
+                        type="email"
+                        {...register("email")}
+                    />
+                    <Input
+                        label="Senha"
+                        name="password"
+                        type="password"
+                        {...register("password")}
+                    />
                 </Stack>
 
                 <Button
@@ -23,6 +49,7 @@ export default function SignIn() {
                     marginTop="6"
                     colorScheme="pink"
                     size="lg"
+                    isLoading={formState.isSubmitting}
                 >
                     Entrar
                 </Button>
